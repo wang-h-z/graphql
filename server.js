04e82f477b3dd3cd810731ae198f5b54f4eab601
haozhen <whaozhen01@gmail.com>
2025-08-28 16:26:00 +0800

    Create basic graphql schema and query

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 
 const app = express();
+
+// Define a basic schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'HelloWorld',
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello World!'
+      }
+    })
+  })
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
 app.listen(8080, () => console.log("Server running on port 8080"));
